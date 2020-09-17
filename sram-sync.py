91c2b7ca00f5ddcdb27a76bad6db8cdffbafa1b6
Jan-Erek Thiede <j.thiede@maastrichtuniversity.nl>
2020-09-17 19:44:32 +0200

    removes LDAP class sczGroup and posixGroup, replaces with groupOfNames. Minor changes in logging [ DHS-757 | DHS-824 ]

--- sram-sync.py
+++ sram-sync.py
@@ -12,7 +12,7 @@ import ldap
 
 from irods.column import Criterion
 from irods.exception import PycommandsException, iRODSException, UserDoesNotExist, UserGroupDoesNotExist, \
-    CATALOG_ALREADY_HAS_ITEM_BY_THAT_NAME
+    CAT_INVALID_GROUP, CATALOG_ALREADY_HAS_ITEM_BY_THAT_NAME
 from irods.models import User, UserGroup
 from irods.user import iRODSUser, iRODSUserGroup
 
@@ -44,8 +44,8 @@ LDAP_GROUPS_BASE_DN = os.environ['LDAP_GROUPS_BASE_DN']
 
 LDAP_SERVICE_PREFIX = "datahubmaastricht:"
 LDAP_USERS_SEARCH_FILTER = "(objectClass=person)"
-LDAP_GROUPS_SEARCH_FILTER = "(objectClass=posixGroup)"
-LDAP_GROUP_MEMBER_ATTR = 'sczMember'
+LDAP_GROUPS_SEARCH_FILTER = "(objectClass=groupOfNames)"
+LDAP_GROUP_MEMBER_ATTR = 'member'
 
 LDAP_COS_BASE_DN = os.environ['LDAP_COS_BASE_DN']
 LDAP_COS_SEARCH_FILTER = "(objectClass=organization)"
@@ -235,18 +235,28 @@ class LdapGroup:
     # ---
     @classmethod
     def create_for_ldap_entry(cls, ldap_entry):
-        group_name = read_ldap_attribute(ldap_entry, 'cn')
+        group_name = read_ldap_attribute(ldap_entry, 'cn')       
+        
+        # Focus on CO groups only.
+        # If we need to sync other groups as well, we need to adjust for the ':' character,
+        # since iRODS will fail on groupnames with that character!
+
+        if len(group_name.split(':')) > 1:
+            return None
+
         # get us an array of all member-attributes, which contains
         # DNs: [ b'cn=empty-membership-placeholder',  b'uid=p.vanschayck@maastrichtuniversity.nl,ou=users,dc=datahubmaastricht,dc=nl', ...]
         group_member_dns = ldap_entry.get(LDAP_GROUP_MEMBER_ATTR, [b""])
         group_member_uids = list(
-            filter(lambda x: x is not None, map(LdapGroup.get_group_member_uids, group_member_dns)))
+            filter(lambda x: x is not None, map(LdapGroup.get_group_member_uids, group_member_dns))) 
+            
         return LdapGroup(group_name, group_member_uids)
 
     # ---
     def create_new_group(self, irods_session, dry_run):
         if dry_run:
             return
+        logger.info("Creating group: {}".format(self.group_name))
         new_group = irods_session.user_groups.create(self.group_name)
         if self.display_name:
             new_group.metadata.add(GroupAVU.DISPLAY_NAME.value, self.display_name)
@@ -464,9 +474,12 @@ def get_ldap_groups(l, group_key_2_co):
                                       LdapGroup.create_for_ldap_entry)
 
     for group in ldap_groups:
-        # dangerous: by
-        co_key = group.group_name.split(":")[0]
-        group_name_2_groups[group.group_name] = group
+        # dangerous: string parsing!
+        logger.info("LDAP Group: {}".format(group.group_name))
+        co_key = group.group_name.split(":")[0]   
+        #Test this change!
+        #group_name_2_groups[group.group_name] = group
+        group_name_2_groups[co_key] = group
 
     return group_name_2_groups
 
@@ -642,6 +655,7 @@ def main(dry_run):
 def sigterm_handler(_signal, _stack_frame):
     sys.exit(0)
 
+SLEEP_INTERVAL_MINUTES=0.5
 
 if __name__ == "__main__":
     # Handle the SIGTERM signal from Docker
@@ -652,7 +666,9 @@ if __name__ == "__main__":
         exit_code = main(not settings.commit)
         if settings.scheduled:
             while True:
-                time.sleep(5 * 60)
+                seconds = int(SLEEP_INTERVAL_MINUTES * 60)
+                logger.info("Sleeping for {} seconds".format(seconds))
+                time.sleep( seconds )
                 main(not settings.commit)
         sys.exit(exit_code)
     finally:
