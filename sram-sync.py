b69d372738bb7cbf8f257841ed664ed201fbbfed
Dean Linssen <linssen.ddj@gmail.com>
2022-12-06 11:08:13 +0100

    Add check to see if user with email address already exists in iRODS [DHDO-723]

--- sram-sync.py
+++ sram-sync.py
@@ -228,6 +228,11 @@ class LdapUser:
         if self.email:
             new_irods_user.metadata.add(UserAVU.EMAIL.value, self.email)
             logger.info("-- user {} added AVU: {} {}".format(self.uid, UserAVU.EMAIL.value, self.email))
+            try:
+                irods_session.users.get(self.email)
+                print("-- user with this email address already exists in iRODS")
+            except UserDoesNotExist:
+                print("-- user {} did not previously exist in iRODS".format(self.uid))
         if self.display_name:
             new_irods_user.metadata.add(UserAVU.DISPLAY_NAME.value, self.display_name)
             logger.info("-- user {} added AVU: {} {}".format(self.uid, UserAVU.DISPLAY_NAME.value, self.display_name))
